33214e881349c24970e16b130e72dbdfeb298694
claire <clairebontempo@gmail.com>
2021-01-05 15:41:41 -0800

    commit tree d4a4489e43015efe7431c1fc52b00b6b5373e5c5

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,6 @@
 class Author < ActiveRecord::Base
+    validates :name, presence: true #does this automatically validate with the inclusion over other validations?
+    validates :name, uniqueness: true
+    validates :phone_number, length: { is: 10 }
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-fiction) }
+    validate :title_contains_clickbait
+
+
+
+    def title_contains_clickbait
+        if self.title?
+            unless self.title.match /Won't Believe|Secret|Guess|Top \d/
+                errors.add(:title, "must be clickbait")
+            end
+        end
+    end
 end
